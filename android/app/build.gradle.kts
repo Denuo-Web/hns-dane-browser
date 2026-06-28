4c2ca68b8e3c7ccc66b31667301ce476f7d25b44
Jaron Rosenau <jaron.rosenau@gmail.com>
2026-06-28 00:40:55 -0700

    Release 0.1.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.handshake.browser"
         minSdk = 34
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.1.6"
+        versionCode = 8
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
