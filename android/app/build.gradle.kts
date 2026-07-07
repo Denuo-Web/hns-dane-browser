d3eaa56682d9832f5ea8f046b69de0d87ad97000
Jaron Rosenau <jaron.rosenau@gmail.com>
2026-07-07 00:39:36 -0700

    Release version 0.3.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.denuoweb.hnsdane"
         minSdk = 34
         targetSdk = 37
-        versionCode = 23
-        versionName = "0.3.2"
+        versionCode = 24
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
